f89f01f2467c6cdfe59b2cf16a3f1539d3e8e077
Martin Greenwood <martin@pixelpudu.com>
2016-05-16 12:24:24 +0100

    fix JS issue

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ var $j = jQuery;
 
 // SCROLL TO #
 $j(function($) {
-	$j('a[href*=#]:not([href=#])').click(function() {
+	$j('a[href*=\\#]:not([href=\\#])').click(function() {
 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 			var target = $j(this.hash);
 			target = target.length ? target : $j('[name=' + this.hash.slice(1) +']');
